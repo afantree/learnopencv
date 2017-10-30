66be2510be27e9c4e7517dcb94a99f1afdbad3b6
阿凡树 <729397005@qq.com>
2017-10-30 15:14:47 +0800

    update

--- opencv/FaceRecognizer1/main.cpp
+++ opencv/FaceRecognizer1/main.cpp
@@ -26,16 +26,22 @@ int main(int argc, const char * argv[]) {
     
     CascadeClassifier eyeCascade;
     CascadeClassifier faceCascade;
+    CascadeClassifier noseCascade;
+    CascadeClassifier mouthCascade;
     bool stop = false;
     //训练好的文件名称，放置在可执行文件同目录下
     eyeCascade.load("/Volumes/extension/githubmy/learnopencv/opencv/FaceRecognizer1/haarcascade_eye.xml");
     faceCascade.load("/Volumes/extension/githubmy/learnopencv/opencv/FaceRecognizer1/haarcascade_frontalface_alt.xml");
+    noseCascade.load("/Volumes/extension/githubmy/learnopencv/opencv/FaceRecognizer1/haarcascade_mcs_nose.xml");
+    mouthCascade.load("/Volumes/extension/githubmy/learnopencv/opencv/FaceRecognizer1/haarcascade_mcs_mouth.xml");
     while (!stop)
     {
         cap >> frame;
         //建立用于存放人脸的向量容器
         vector<Rect> faces(0);
         vector<Rect> eyes(0);
+        vector<Rect> nose(0);
+        vector<Rect> mouth(0);
         cvtColor(frame, gray, CV_BGR2GRAY);
         //变换后的图像进行直方图均值化处理
         equalizeHist(gray, gray);
@@ -50,6 +56,16 @@ int main(int argc, const char * argv[]) {
                                     | CV_HAAR_DO_ROUGH_SEARCH
                                     | CV_HAAR_SCALE_IMAGE,
                                     Size(100, 100));
+        noseCascade.detectMultiScale(gray, nose, 1.1, 2,0
+                                    | CV_HAAR_FIND_BIGGEST_OBJECT
+                                    | CV_HAAR_DO_ROUGH_SEARCH
+                                    | CV_HAAR_SCALE_IMAGE,
+                                    Size(50, 50));
+        mouthCascade.detectMultiScale(gray, mouth, 1.1, 2,0
+                                    | CV_HAAR_FIND_BIGGEST_OBJECT
+                                    | CV_HAAR_DO_ROUGH_SEARCH
+                                    | CV_HAAR_SCALE_IMAGE,
+                                    Size(50, 50));
         // 用红色的线画出眼睛的框
         for (size_t i = 0;i<eyes.size(); i++)
         {
@@ -64,6 +80,22 @@ int main(int argc, const char * argv[]) {
             if (faces[i].height > 0 && faces[i].width > 0)
             {
                 rectangle(frame, faces[i], Scalar(0, 255, 0), 1, 8, 0);
+            }
+        }
+        // 用青色的线画出脸的框
+        for (size_t i = 0;i<nose.size(); i++)
+        {
+            if (nose[i].height > 0 && nose[i].width > 0)
+            {
+                rectangle(frame, nose[i], Scalar(255, 255, 0), 1, 8, 0);
+            }
+        }
+        // 用黄色的线画出脸的框
+        for (size_t i = 0;i<mouth.size(); i++)
+        {
+            if (mouth[i].height > 0 && mouth[i].width > 0)
+            {
+                rectangle(frame, mouth[i], Scalar(0, 255, 255), 1, 8, 0);
             }
         }
         imshow("face", frame);
